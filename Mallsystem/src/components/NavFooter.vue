e327d3e1e3ec49d4500afae84c933b4c4fcc612d
LiuYu <136351626@qq.com>
2018-12-13 13:47:23 +0800

    Foreground page

---
+++ Mallsystem/src/components/NavFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer class="footer">
+    <div class="footer__wrap">
+      <div class="footer__secondary">
+        <div class="footer__inner">
+          <div class="footer__region">
+            <span>Author:</span>
+            <span>LiuYu</span>
+          </div>
+          <div class="footer__secondary__nav">
+            <span>Copyright © 2017 IMooc All Rights Reserved.</span>
+            <a href="http://us.lemall.com/us/aboutUs.html">
+              About Us
+            </a>
+            <a href="http://us.lemall.com/us/termsofUse.html">
+              Terms &amp; Conditions
+            </a>
+            <a href="http://us.lemall.com/us/privacyPolicy.html">
+              Privacy Policy
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+    export default {
+        name: "NavFooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ Mallsystem/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import GoodsList from '@/views/GoodsList'
+import Cart from '@/views/Cart'
+import Address from '@/views/Address'
+import Orderconfirm from '@/views/orderconfirm'
+import OrderSuccess from '@/views/orderSuccess'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'GoodsList',
+      component: GoodsList
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/address',
+      name: 'Address',
+      component: Address
+    },
+    {
+      path: '/orderconfirm',
+      name: 'Orderconfirm',
+      component: Orderconfirm
+    },
+    {
+      path: '/orderSuccess',
+      name: 'OrderSuccess',
+      component: OrderSuccess
+    }
+  ]
+})
